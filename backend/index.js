15f0a53ef0f331e67987c558538c35ee78273c71
lamooon <111581657+lamooon@users.noreply.github.com>
2024-02-20 12:09:08 +0900

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+const port = 8080;
+const router = express.Router();
+
+const htmlFiles = fs.readdirSync('./pages', {withFileTypes: true})
+    .map(file => file.name);
+
+app.use(express.static('static'));
+app.use(express.static('static/logo'));
+
+fs.readFile('./pages/404.html', (err, data) => {
+    let errorPage = data;
+});
+
+htmlFiles.forEach(file => {
+    let route = '/' + file.slice(0, -5);
+    console.log(route);
+    route = file === 'landing-page.html' ? '/' : route;
+    router.get(route, (req, res) => {
+        fs.readFile(`./pages/${file}`, (err, data) => {
+            if (err) {
+                console.log("hi?", err.message);
+            }
+            else {
+                res.status(200).set('Content-Type', 'text/html');
+                res.send(data);
+                res.end();
+            }
+        });
+    });
+});
+
+app.use('/', router);
+
+app.listen(port, function () {
+    console.log(`Listening at port ${port}`);
+});
